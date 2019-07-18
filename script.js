4d2e90c01f6046e25801ff664cbed87fdbf1154e
MichaelOppong <michael@scripted.org>
2019-07-18 12:08:27 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".mrpiffls").click(function(){
+    $(".disappear").toggle();
+});
+$(".piff").click(function(){
+    $(".me").fadeToggle();
+});
+$(".bye").click(function(){
+    $(".person").slideUp();
+});
+$(".hi").click(function(){
+    $(".appear").slideDown();
+});
+$(".float").click(function(){
+    $(".penny").toggle();
+});
